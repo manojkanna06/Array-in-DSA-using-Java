f2328b74fec7a33637b14aafbb3070881c66bbd3
manojkanna06 <rmanojkanna1106@gmail.com>
2026-09-09 10:55:24 +0530

    Update ArrayBasics.java

--- Day01_Arrays_Basics/ArrayBasics.java
+++ Day01_Arrays_Basics/ArrayBasics.java
@@ -1,8 +1,6 @@
 class Main {
     public static void main(String[] args) {
-
         int[] arr = {10, 20, 30, 40, 50};
-
         System.out.println(arr[0]);
         System.out.println(arr[1]);
         System.out.println(arr[2]);
