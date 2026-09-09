40798acba1ef198a64555dd79decc994a7d50a1f
manojkanna06 <rmanojkanna1106@gmail.com>
2026-09-09 14:52:40 +0530

    Update Array_with_user_input.java

--- Day01_Arrays_Basics/Array_with_user_input.java
+++ Day01_Arrays_Basics/Array_with_user_input.java
@@ -8,12 +8,12 @@ public class Array_with_user_input {
         int n = sc.nextInt();
         int[] arr = new int[n + 1];
         System.out.println("Enter array values:");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
         // Traversal
         System.out.println("Original array:");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++){
             System.out.print(arr[i] + " ");
         }
         // Insertion
@@ -21,34 +21,41 @@ public class Array_with_user_input {
         int position = sc.nextInt();
         System.out.print("Enter value to insert: ");
         int value = sc.nextInt();
-        for (int i = n; i > position; i--) {
+        for (int i = n; i > position; i--){
             arr[i] = arr[i - 1];
         }
         arr[position] = value;
         n++;
         System.out.println("After insertion:");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++){
             System.out.print(arr[i] + " ");
         }
         // Deletion
         System.out.print("\nEnter position to delete: ");
         position = sc.nextInt();
-        for (int i = position; i < n - 1; i++) {
+        for (int i = position; i < n - 1; i++){
             arr[i] = arr[i + 1];
         }
         n--;
         System.out.println("After deletion:");
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
-        }int max = arr[0];
-
-for (int i = 1; i <=arr.length-1; i++) {
-    if (arr[i] > max) {
-        max = arr[i];
+        }
+        //max
+        int max = arr[0];
+        for (int i = 1; i <=arr.length-1; i++){     
+               if (arr[i] > max) {
+                     max=arr[i];
     }
 }
-
-System.out.println("Maximum = " + max);
-        
+            System.out.println("Maximum=" + max);
+       //min
+            int min=arr[0];
+            for(int i=0;i<=arr.length;i++){
+                  if(arr[i]<min){
+                        min=arr[i];
+                  }
+            }
+            System.out.println("Minimum="+min);     
     }
 }
