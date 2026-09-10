73cae1c66644a340162362636151af06d89bf0e9
manojkanna06 <rmanojkanna1106@gmail.com>
2026-09-10 10:34:49 +0530

    Update BubbleSorting.java

--- sorting/BubbleSorting.java
+++ sorting/BubbleSorting.java
@@ -1,23 +1,16 @@
 class BubbleSorting {
     public static void main(String[] args) {
-
         int[] arr = {40, 10, 30, 20};
-
         for (int i = 0; i < arr.length - 1; i++) {
-
             for (int j = 0; j < arr.length - 1 - i; j++) {
-
                 if (arr[j] > arr[j + 1]) {
-
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
             }
         }
-
         System.out.println("Sorted array:");
-
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
