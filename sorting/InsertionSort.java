74701a17a5de230074954606b6e6a893f573b61a
manojkanna06 <rmanojkanna1106@gmail.com>
2026-09-10 11:54:02 +0530

    Update InsertionSort.java

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -1,1 +1,18 @@
-
+class InsertionSorting {
+    public static void main(String[] args) {
+        int[] arr = {40, 10, 30, 20};
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        System.out.println("Sorted array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
