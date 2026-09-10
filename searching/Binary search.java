34e261d07920cfe6d2aaa659a78174c2d936b07b
manojkanna06 <rmanojkanna1106@gmail.com>
2026-09-10 10:01:01 +0530

    Update Binary search.java

--- searching/Binary search.java
+++ searching/Binary search.java
@@ -1,1 +1,26 @@
-
+class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+        int target = 60;
+        int left = 0;
+        int right = arr.length - 1;
+        boolean found = false;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                System.out.println("Element found at index " + mid);
+                found = true;
+                break;
+            }
+            else if (target > arr[mid]) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        if (!found) {
+            System.out.println("Element not found");
+        }
+    }
+}
