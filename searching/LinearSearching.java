a076be7ca3b87a05403d635829e6aa7258f1f40d
manojkanna06 <rmanojkanna1106@gmail.com>
2026-09-10 10:13:30 +0530

    Update LinearSearching.java

--- searching/LinearSearching.java
+++ searching/LinearSearching.java
@@ -1,1 +1,12 @@
-
+boolean found=false;
+for(int i=0;i<=arr.length-1;i++){
+  if(arr[i]==key){
+    System.out.print(i);
+    found=true;
+    break;
+  }
+}
+if(!found){
+  System.out.println("not found");
+}
+  
